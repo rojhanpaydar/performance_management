9d8cf44b8540ee9fc3432dbb18e4f262559f164d
Rojhan Paydar <rojhanpaydar@gmail.com>
2020-08-07 13:40:55 -0400

    Updates

---
+++ precache-manifest.da162553ace0f32d7d2bd98dc6bf2f01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9da66cc9bd8c85b0292c67055c56cc86",
+    "url": "/performance_management/index.html"
+  },
+  {
+    "revision": "21616e4f62997f723fbd",
+    "url": "/performance_management/static/css/main.c142252f.chunk.css"
+  },
+  {
+    "revision": "210bcd689664f3687df1",
+    "url": "/performance_management/static/js/2.a9e2b928.chunk.js"
+  },
+  {
+    "revision": "4f44a17bb08e78ed1b92485defc539ac",
+    "url": "/performance_management/static/js/2.a9e2b928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21616e4f62997f723fbd",
+    "url": "/performance_management/static/js/main.dc292e29.chunk.js"
+  },
+  {
+    "revision": "693e33d260f30ea68dc6",
+    "url": "/performance_management/static/js/runtime-main.092c43cd.js"
+  }
+]);
